99a0cc0a2e7afea4e932bd71ec87fbbd74d65fb2
Shon Garrison <smgarrison05@gmail.com>
2025-08-29 13:45:36 -0500

    Started Framework
    Started by placing buttons and labels.

--- Training_Funds_Traker_tk.py
+++ Training_Funds_Traker_tk.py
@@ -21,16 +21,35 @@ window.style = Style(theme = "darkly")
 
 # Main Window Framework ==========================================
 
+
 # Input Framework ================================================
 
 
 # Output Framework ===============================================
-
+lbl_Line = tk.Label(window, text="__________", font="Calibri, 16")
+lbl_Line.place(x=695, y=182)
+lbl_Operator = tk.Label(window, text="(+/-)", font="Calibri, 16")
+lbl_Operator.place(x=647, y=166)
+label1 = tk.Label(window, text="Preview of Current Transaction:")
+label1.place(x=25, y=251)
+Prev_Label = tk.Text(window, bg="#DAD4D4")  # TODO: Check for text or label for this fill
+Prev_Label.place(x=25,y=272, width=791, height=113 )
 
 # Button Framework ===============================================
-
+button = tk.Button(window, text = "Clear", command=lambda: print("Clear Button was pressed"))
+button.place(x=25, y=420, width=86, height=41)
+button1 = tk.Button(window, text = "Calculate", command=lambda: print("Calculate Button was pressed"))
+button1.place(x=186, y=420, width=86, height=41)
+button2 = tk.Button(window, text = "Apply", command=lambda: print("Apply Button was pressed"))
+button2.place(x=366, y=420, width=86, height=41)
+button3 = tk.Button(window, text = "Reconcile", command=lambda: print("Reconcile Button was pressed"))
+button3.place(x=546, y=420, width=86, height=41)
+button4 = tk.Button(window, text = "Close", command=window.destroy)
+button4.place(x=730, y=420, width=86, height=41)
 
 
 # Run Application ================================================
 window.mainloop()
+print("\nApplication Terminated")
+print("Good-bye")
 
